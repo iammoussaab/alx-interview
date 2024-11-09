f4e622c3bba24285a115f6ce7fd5478cb48a78c3
iammoussaab <iammoussaab@gmail.com>
2024-11-09 10:54:50 +0100

    chill out

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,40 +4,37 @@ import sys
 
 
 def print_board(board, n):
-    """Print allocated positions to the queen"""
-    b = []
-
+    """Print allocated positions of the queens"""
+    result = []
     for i in range(n):
-        for j in range(n):
-            if j == board[i]:
-                b.append([i, j])
-    print(b)
+        result.append([i, board[i]])
+    print(result)
 
 
-def is_position_safe(board, i, j, r):
-    """Checks if the position is safe for the queen"""
-    return board[i] in (j, j - i + r, i - r + j)
+def is_position_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row] = col"""
+    for i in range(row):
+        # Check column and diagonal conflicts
+        if board[i] == col or abs(board[i] - col) == abs(i - row):
+            return False
+    return True
 
 
 def safe_positions(board, row, n):
-    """Find all safe positions where the queen can be allocated"""
+    """Find all safe positions for queens and print solutions"""
     if row == n:
         print_board(board, n)
-
     else:
-        for j in range(n):
-            allowed = True
-            for i in range(row):
-                if is_position_safe(board, i, j, row):
-                    allowed = False
-            if allowed:
-                board[row] = j
+        for col in range(n):
+            if is_position_safe(board, row, col):
+                board[row] = col
                 safe_positions(board, row + 1, n)
+                board[row] = -1  # Reset position (backtrack)
 
 
 def create_board(size):
     """Generates the board"""
-    return [0 * size for i in range(size)]
+    return [-1] * size
 
 
 if len(sys.argv) != 2:
@@ -46,14 +43,13 @@ if len(sys.argv) != 2:
 
 try:
     n = int(sys.argv[1])
-except BaseException:
+except ValueError:
     print("N must be a number")
     exit(1)
 
-if (n < 4):
+if n < 4:
     print("N must be at least 4")
     exit(1)
 
-board = create_board(int(n))
-row = 0
-safe_positions(board, row, int(n))
+board = create_board(n)
+safe_positions(board, 0, n)
