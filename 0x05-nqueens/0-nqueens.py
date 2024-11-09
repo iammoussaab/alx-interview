9e9e2ba07f09a1df39c6b5dda574892a1d9a1578
iammoussaab <iammoussaab@gmail.com>
2024-11-09 11:18:06 +0100

    check again wih another things

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,87 +1,61 @@
 #!/usr/bin/python3
-"""N Queens solution finder module.
-"""
+"""N Queens placement on NxN chessboard"""
+
+
 import sys
 
 
-solutions = []
-"""The list of possible solutions to the N queens problem.
-"""
-n = 0
-"""The size of the chessboard.
-"""
-board = None
-"""The list representing the chessboard for queen placement.
-"""
+def gene_solution(row, column):
+    solution = [[]]
+    for queen in range(row):
+        solution = place_queen(queen, column, solution)
+    return solution
 
 
-def get_input():
-    """Retrieves and validates this program's argument.
+def place_queen(queen, column, prev_solution):
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
 
-    Returns:
-        int: The size of the chessboard.
-    """
-    global n
+
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
+
+
+def init():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    try:
+    if sys.argv[1].isdigit():
         n = int(sys.argv[1])
-    except ValueError:
+    else:
         print("N must be a number")
         sys.exit(1)
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    return n
+    return (n)
 
 
-def is_position_safe(board, row, col):
-    """Checks if a queen can be safely placed at the given position.
+def n_queens():
 
-    Args:
-        board (list): The board with queen placements.
-        row (int): The row index.
-        col (int): The column index.
-
-    Returns:
-        bool: True if the position is safe, otherwise False.
-    """
-    for i in range(row):
-        # Check column and both diagonals
-        if board[i] == col or abs(board[i] - col) == abs(i - row):
-            return False
-    return True
+    n = init()
+    # generate all solutions
+    solutions = gene_solution(n, n)
+    # print solutions
+    for array in solutions:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
 
 
-def solve_nqueens(row):
-    """Recursively attempts to solve the N Queens problem.
-
-    Args:
-        row (int): The current row to attempt placing a queen.
-    """
-    global solutions, board, n
-    if row == n:
-        # If all queens are placed, add the solution
-        solutions.append([[i, board[i]] for i in range(n)])
-    else:
-        for col in range(n):
-            if is_position_safe(board, row, col):
-                board[row] = col
-                solve_nqueens(row + 1)
-                # Backtrack
-                board[row] = -1
-
-
-def get_solutions():
-    """Sets up the board and starts the solution search."""
-    global board, n
-    board = [-1] * n
-    solve_nqueens(0)
-
-
-# Main execution
-n = get_input()
-get_solutions()
-for solution in solutions:
-    print(solution)
+if __name__ == '__main__':
+    n_queens()
