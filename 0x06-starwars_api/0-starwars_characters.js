acbd3f0f6bf6d288d60a693731300818022aeac6
iammoussaab <iammoussaab@gmail.com>
2024-11-20 15:49:22 +0100

    check this with recursive

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,22 @@
 #!/usr/bin/node
 
-const request = require("request");
+import request from "request";
 
-const id = process.argv[2] + "/";
+const id = process.argv[2];
 const url = "https://swapi-api.hbtn.io/api/films/";
 
-request(url + id, async function (err, res, body) {
-  if (err) return console.error(err);
+request(url + id, function (err, res, body) {
+  if (err) throw err;
 
-  const data = JSON.parse(body);
-  const listUrl = data.characters;
-  
-  for (const li of listUrl) {
-    await new Promise(function (resolve, reject) {
-      request(li, function (err, res, body) {
-        if (err) return console.error(err);
-
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
