3aefcff90f8d3ea27cf54499437becf8a719dd44
iammoussaab <iammoussaab@gmail.com>
2024-11-20 15:42:23 +0100

    check again

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,8 +8,9 @@ const url = "https://swapi-api.hbtn.io/api/films/";
 request(url + id, async function (err, res, body) {
   if (err) return console.error(err);
 
-  const listUrl = JSON.parse(body).characters;
-
+  const data = JSON.parse(body);
+  const listUrl = data.characters;
+  
   for (const li of listUrl) {
     await new Promise(function (resolve, reject) {
       request(li, function (err, res, body) {
